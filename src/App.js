330a3edd17d88c4b00bb98b3f77cd69a45bdebd6
unknown <romagleba@gmail.comgit config --global user.name RomanGleba>
2024-04-28 16:28:31 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React  from 'react';
+import './App.css';
+
+
+
+
+
+
+const App = () => {
+ return (
+ <div className='app-wrapper'>
+   <header>
+      <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR814Wk7AUXXEROar2uxUMkAR55GOKN-qPkS6FVtyaHRg&s'/>
+   </header>
+    <nav>
+      <div>
+         Profile
+      </div>
+      <div>Messages</div>
+      </nav> 
+      <div>
+         Main content
+      </div>
+    
+      </div>);
+      
+}         
+
+
+       
+
+export default App;

c7a1ed120d4f9307ffaead1fdc916d2128f155e4
romagleba@gmail.com <romagleba@gmail.comgit config --global user.name RomanGleba>
2024-04-29 16:05:25 +0300

    add alt

--- src/App.js
+++ src/App.js
@@ -9,20 +9,49 @@ import './App.css';
 const App = () => {
  return (
  <div className='app-wrapper'>
-   <header>
+   <header className='header'>
       <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR814Wk7AUXXEROar2uxUMkAR55GOKN-qPkS6FVtyaHRg&s'/>
-   </header>
-    <nav>
+      </header>
+      <nav classname='nav'> 
       <div>
-         Profile
+         <a>Profil</a>
       </div>
-      <div>Messages</div>
-      </nav> 
       <div>
-        
-      </div>
-       <div> public content Developer Pro</div>
+      <a>Messages</a>
+      </div><div>
+         <a>News</a>
+         </div><div>
+            <a>Music</a>
+            </div>
+            <div>
+               Setting
+            </div>
        
+      
+      </nav>
+      <div classname='content'> 
+      <div>
+   
+         <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHoCwNIsQabEhDUTO4KkpjIH-azEAz9REaVA&usqp=CAU'/>
+      </div>
+      <div>
+         avatar + description
+      </div>
+      <div>
+         My posts
+      </div>
+      <div>
+         New posts
+      </div>
+      <div>
+         <div>
+            New post
+         </div>
+         <div>
+            post1
+         </div>
+      </div>
+      </div>
       </div>);
       
 }         

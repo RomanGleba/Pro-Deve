530d592119d05e94f587918bd7ab6d2eb4184954
romagleba@gmail.com <romagleba@gmail.comgit config --global user.name RomanGleba>
2024-04-29 20:48:16 +0300

    git-Developer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React  from 'react';
 import './App.css';
+import Header from './Components/Header';
+import Navbar from './Components/Navbar';
+import Profile from './Components/Profile';
 
 
 
@@ -9,49 +12,12 @@ import './App.css';
 const App = () => {
  return (
  <div className='app-wrapper'>
-   <header className='header'>
-      <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR814Wk7AUXXEROar2uxUMkAR55GOKN-qPkS6FVtyaHRg&s'/>
-      </header>
-      <nav classname='nav'> 
-      <div>
-         <a>Profil</a>
-      </div>
-      <div>
-      <a>Messages</a>
-      </div><div>
-         <a>News</a>
-         </div><div>
-            <a>Music</a>
-            </div>
-            <div>
-               Setting
-            </div>
-       
+   <Header />
+   <Navbar />
+   <Profile />
+
+    
       
-      </nav>
-      <div classname='content'> 
-      <div>
-   
-         <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHoCwNIsQabEhDUTO4KkpjIH-azEAz9REaVA&usqp=CAU'/>
-      </div>
-      <div>
-         avatar + description
-      </div>
-      <div>
-         My posts
-      </div>
-      <div>
-         New posts
-      </div>
-      <div>
-         <div>
-            New post
-         </div>
-         <div>
-            post1
-         </div>
-      </div>
-      </div>
       </div>);
       
 }         
